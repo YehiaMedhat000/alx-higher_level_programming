ef88a01dcae41b24ff311b037ff5140327dfaeac
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2024-03-12 14:28:11 +0200

    12-fizzbuzz.py: Fixed the output form

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,10 +2,10 @@
 def fizzbuzz():
     for i in range(1, 101):
         if not (i % 5) and not (i % 3):
-            print("Fizz", end=" ")
+            print("FizzBuzz", end=" ")
         elif not (i % 5):
             print("Buzz", end=" ")
         elif not (i % 3):
-            print("FizzBuzz", end=" ")
+            print("Fizz", end=" ")
         else:
             print(i, end=" ")
