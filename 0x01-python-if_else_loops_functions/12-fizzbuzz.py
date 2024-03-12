975c04cf32ef0310f86e457f5c015bcece5979d2
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2024-03-12 14:23:11 +0200

    12-fizzbuzz.py: Added the 13th task

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(101):
+        if not (i % 3):
+            print("Fizz", end=" ")
+        elif not (i % 5):
+            print("Buzz", end=" ")
+        elif not (i % 5) and not (i % 3):
+            print("FizzBuzz", end=" ")
+        else:
+            print(i, end=" ")
