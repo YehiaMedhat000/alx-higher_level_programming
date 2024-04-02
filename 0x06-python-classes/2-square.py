df92ea16476b47db5abec404468f767f60705c3d
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2024-04-02 17:20:01 +0200

    Added another solution hope it works

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
-"""
-    Defines class called Square
-"""
+"""Defines a class Square"""
 
 
 class Square:
     """
-        The definition of the square class
+    Class that defines properties of square by: (based on 1-square.py).
 
-        Attributes:
-            size: The side length of the square
+    Attributes:
+        size: size of a square (1 side).
     """
-    def __init__(self, size=0) -> None:
+    def __init__(self, size=0):
+        """Creates new instances of square.
+
+        Args:
+            size: size of the square (1 side).
+        """
         self.__size = size
 
-        if self.__size < 0:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif not isinstance(size, int):
+        elif size < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
