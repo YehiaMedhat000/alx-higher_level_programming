c3ff2795f3a30da3cc11f6e9b1f0ea13aecd3107
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2024-04-02 17:03:46 +0200

    1-square.py: The 2nd task

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    Defines class called Square
+"""
+
+
+class Square:
+    """
+        The definition of the square class
+    """
+    def __init__(self, size) -> None:
+        self.__size = size
+
+
