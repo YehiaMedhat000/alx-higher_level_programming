f210ab08abf2bef0ba7b6c686537397c0012ffbf
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2024-04-08 06:35:55 +0200

    4-print_square.py: Added the documenation

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 def print_square(size):
 
+    """
+    Prints a square using the # char
+
+    Args:
+    size -- Integer side length of the square
+    """
+
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
-
-    # if size is a float and is less than 0
-    # raise a TypeError exception with the message size must be an integer
     elif size < 0:
         raise ValueError("size must be >= 0")
 
