5bcf660b39abec1c38bf58e827238f7d181ace48
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2024-04-09 16:52:39 +0200

    101-locked_class.py: Added the 2nd advanced task

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""
+Class definition that prevents creating attributes
+"""
+class LockedClass:
+    """
+    LockedClass class definition
+    Accepts only the attribute `first_name`
+    """
+    __slots__ = ["first_name"]
