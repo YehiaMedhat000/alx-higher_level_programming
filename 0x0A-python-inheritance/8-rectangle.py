324ef161e9525549e6db782c561f06d0f4e25491
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2024-04-15 19:36:04 +0200

    Added the 9th task

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Module defining Rectangle class based on 
+    `BaseGeometry` class
+"""
+
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+class Rectangle(BaseGeometry):
+    """ Class Definition `Rectangle` """
+
+    def __init__(self, width, height):
+        """ Initialization function
+            called whenever a new instance of `Rectangle` is made
+
+            Args:
+                self -- The object
+                width -- The width of the object
+                height -- The height of the object
+        """
+        super().__init__()
+        self.integer_validator('width', width)
+        self.__width = width
+        self.integer_validator('height', height)
+        self.__height = height
