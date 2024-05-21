74b017bb8d00b66158c1d79436caf592fb568f22
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2024-05-21 14:59:31 +0300

    Added the basic files, the first task
    * The README.md file
    * The first .sql script

---
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -0,0 +1,3 @@
+-- Task: List all databases on MySQL server
+-- Connect to MySQL server and execute SHOW DATABASES query
+SHOW DATABASES;
---
+++ 0x0D-SQL_introduction/README.md
@@ -0,0 +1,1 @@
+# Dir for the 0x0D-SQL_introduction project
