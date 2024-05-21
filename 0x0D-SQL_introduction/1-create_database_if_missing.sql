5a9e43d5bc461319c55f5046c48a3e1591691dba
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2024-05-21 15:06:41 +0300

    Added the 2nd and 3rd script

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- Task: Create database hbtn_0c_0 if it does not already exist
+-- The query
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+-- Task: Delete database hbtn_0c_0 if it exists
+-- The query
+DROP DATABASE IF EXISTS hbtn_0c_0;
