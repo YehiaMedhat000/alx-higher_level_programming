d366805ddf2de9db83b90174cbb701cee4c709a7
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2024-05-21 15:36:15 +0300

    Readding the 6th script

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,6 +1,3 @@
 -- Task: Print the full description of the table first_table from the database hbtn_0c_0
 -- Select relevant information from the information schema
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
-FROM information_schema.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0'
-AND TABLE_NAME = 'first_table';
+SELECT CREATE TABLE first_table
