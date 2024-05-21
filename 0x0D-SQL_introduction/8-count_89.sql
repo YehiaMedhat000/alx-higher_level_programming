53dd453c9478faee90cd146be7f983a6f9e28014
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2024-05-21 15:46:10 +0300

    Added the 9th script

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,3 @@
+-- Task: Display the number of records with id = 89 in the table first_table
+-- Count the number of records with id = 89
+SELECT COUNT(*) AS record_count FROM first_table WHERE id = 89;
