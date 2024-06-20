4cabca94bcd87201dd9ba8786cec48cfb0374faa
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2024-06-21 01:42:51 +0300

    Added the 11th task

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  const ret = [];
+  let ret = [];
   if (!(list === undefined)) {
     for (let i = list.length; i > 0; i--) {
       ret[list.length - i] = list[i - 1];
