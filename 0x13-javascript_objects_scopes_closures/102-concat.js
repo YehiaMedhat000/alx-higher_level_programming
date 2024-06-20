d2ee52dae2a2aa33f9cb0b5e2b4c464cf58e0891
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2024-06-21 01:51:36 +0300

    Added the last task

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const [, , fileA, fileB, fileC] = process.argv;
+
+fs.readFile(fileA, 'utf8', (err, dataA) => {
+  if (err) throw err;
+
+  fs.readFile(fileB, 'utf8', (err, dataB) => {
+    if (err) throw err;
+
+    const concatenatedData = `${dataA.trim()}\n${dataB.trim()}\n`;
+
+    fs.writeFile(fileC, concatenatedData, (err) => {
+      if (err) throw err;
+      console.log(`Files ${fileA} and ${fileB} concatenated into ${fileC}`);
+    });
+  });
+});
