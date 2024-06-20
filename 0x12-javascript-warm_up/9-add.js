f128f3f1adc2f681b75f5c78195c8c1dcb68e306
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2024-06-21 00:03:31 +0300

    Added the last main script

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -4,8 +4,10 @@ const args = process.argv.slice(2);
 const num1 = parseInt(args[0], 10);
 const num2 = parseInt(args[1], 10);
 
-function add (a, b) {
+function add(a, b) {
   return a + b;
 }
 
 console.log(add(num1, num2));
+
+module.exports = add;
