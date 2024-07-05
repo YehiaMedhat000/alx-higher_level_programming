edbdf0e9691e9ae6dcfd1730533910880afb11bc
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2024-07-05 21:30:08 +0300

    Fixed something of the requirements

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,8 +6,9 @@
 import urllib.request as req
 import sys
 
+if __name__ == "__main__":
 
-url = sys.argv[1]
+    url = sys.argv[1]
 
-with req.urlopen(url) as response:
-    print(response.headers._headers[-3][1])
+    with req.urlopen(url) as response:
+        print(response.headers._headers[-3][1])
