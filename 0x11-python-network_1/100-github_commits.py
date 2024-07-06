d76c9352a9a1d685974cbc7234545b1a2dac5dce
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2024-07-06 12:51:18 +0300

    Added the challenge script

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Script that lists 10 commits using the Github api
+    from newest to oldest """
+import requests as r
+import sys
+from pprint import pp
+
+
+if __name__ == "__main__":
+
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+    headers = {"owner": owner, "repo": repo}
+
+    url = f"https://api.github.com/repos/{owner}/{repo}/commits"
+
+    response = r.get(url, headers=headers)
+    commits = response.json()
+
+    for i in range(10):
+        sha = commits[i].get("sha")
+        committer = commits[i]["commit"]["author"]["name"]
+        print(f"{sha}: {committer}")
+
+
